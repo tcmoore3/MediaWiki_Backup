8fc688107e489aac969305f215f55ee4d543aa52
Tim Moore <mtimc@umich.edu>
2026-02-20 00:10:37 -0500

    Also backup personal wiki in run script

--- run.bash
+++ run.bash
@@ -1,3 +1,4 @@
 ./backup.sh -d ~/sandbox/test-mediawiki-backup/ -w /Applications/MAMP/htdocs/tims-wiki/ -f -p latest
+./backup.sh -d ~/sandbox/test-mediawiki-backup/ -w /Applications/MAMP/htdocs/tims-personal-wiki/ -f -p tims-personal-wiki-latest
 mv ~/sandbox/test-mediawiki-backup/*.gz ~/University\ of\ Michigan\ Dropbox/Timothy\ Moore/tims-wiki-backups/
 cp /Applications/MAMP/htdocs/tims-wiki/LocalSettings.php ~/University\ of\ Michigan\ Dropbox/Timothy\ Moore/tims-wiki-backups/
